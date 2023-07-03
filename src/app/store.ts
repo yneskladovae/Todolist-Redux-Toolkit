c3c0ce994e900032a84802d90d002a4cd06390a2
yneskladovae <yneskladovae27@gmail.com>
2023-07-03 23:21:20 +0300

    refactor: delete unused imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,9 @@ import { tasksReducer } from "../features/TodolistsList/tasks-reducer";
 import { todolistsReducer } from "../features/TodolistsList/todolists-reducer";
 import {
   AnyAction,
-  applyMiddleware,
   combineReducers,
-  createStore,
 } from "redux";
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "../features/Login/auth-reducer";
 import { configureStore } from "@reduxjs/toolkit";
@@ -22,7 +20,7 @@ export const store = configureStore({
   reducer: rootReducer,
 });
 
-export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AppRootStateType = ReturnType<typeof store.getState>;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
