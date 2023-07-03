5f91c76db49f96f6e8d2e9ac99f40110da23ea8a
yneskladovae <yneskladovae27@gmail.com>
2023-07-03 23:11:55 +0300

    refactor: change store, create configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,18 +9,19 @@ import {
 import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "../features/Login/auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolists: todolistsReducer,
   app: appReducer,
   auth: authReducer,
 });
-// непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-// определить автоматически тип всего объекта состояния
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
@@ -30,7 +31,6 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   AnyAction
 >;
 
-// export type AppDispatch = typeof store.dispatch
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
